f1cfbe13c32f58bc09d5fecf422af644e47a1713
nitishgupta5862-ctrl <nitishgupta5862@gmail.com>
2026-06-28 08:09:24 +0530

    Create ConsBSTfromPREANDinorder.java

---
+++ Week_4/Day_5/ConsBSTfromPREANDinorder.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    HashMap<Integer, Integer> map = new HashMap<>();
+    int preIndex = 0;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+
+        return build(preorder, 0, inorder.length - 1);
+    }
+
+    private TreeNode build(int[] preorder, int left, int right) {
+
+        if (left > right) {
+            return null;
+        }
+
+        int rootValue = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootValue);
+
+        int mid = map.get(rootValue);
+
+        root.left = build(preorder, left, mid - 1);
+        root.right = build(preorder, mid + 1, right);
+
+        return root;
+    }
+}
